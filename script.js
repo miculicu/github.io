593f1238df21506d17e9ea73143bed0562fec05a
miculicu <michatsopa@gmail.com>
2025-02-28 13:23:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -40,9 +40,9 @@ function updateCountdown() {
   const days = Math.floor(diff / (1000 * 60 * 60 * 24));
   const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-  
+  const seconds = Math.floor((diff % (1000 * 60 * 60)) / 1000 );
   // Update the timer's content
-  document.getElementById('countdown').textContent = `${days}d ${hours}h ${minutes}m`;
+  document.getElementById('countdown').textContent = `${days}d ${hours}h ${minutes}m ${seconds}s`;
 }
 
 // Update every second for smooth countdown
